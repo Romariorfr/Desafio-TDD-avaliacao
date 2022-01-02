36237fda011ece7f69359b64fc894f9001aea19d
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-01-02 10:37:00 -0300

    insert and delete implementation

--- src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.bds02.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.services.CityService;
@@ -24,12 +30,24 @@ public class CityController {
 	@GetMapping
 	public ResponseEntity<List<CityDTO>> findAll() {
 
-		// PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(),
-		// pageable.getPageSize(), Sort.by("name"));
-
 		List<CityDTO> list = service.findAllPaged();
 		return ResponseEntity.ok().body(list);
 
+	}
+
+	@PostMapping
+	public ResponseEntity<CityDTO> insert(@RequestBody CityDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<CityDTO> delete(@PathVariable Long id) {
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +24,29 @@ public class CityService {
 	public List<CityDTO> findAllPaged() {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+
+	}
+
+	@Transactional
+	public CityDTO insert(CityDTO cityDTO) {
+
+		City entity = new City();
+		entity.setName(cityDTO.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
+
+	@Transactional
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+
+		}
+
+		catch (DataIntegrityViolationException e) {
+
+		}
 
 	}
 
